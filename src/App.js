8d3c14d49a447c48e7827894239e3b4a63c439a2
Alejandro <alejandrocsc@protonmail.com>
2021-04-04 12:03:49 -0700

    Instant Commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ const cubed = x => {
 
 const App = () => {
   return (
-    <div className="tc">
-      <h1 className="bg-light-black dib br3 pa2 ma2 grow bw2 shadow-5">
-        Welcome!
+    <div className="tc bg-gray">
+      <h1 className="bg-black dib br3 pa2 ma2 grow bw2 shadow-5">
+        <p style={{color: 'white'}}>Alejandro Blanco</p>
       </h1>
     </div>
   );
