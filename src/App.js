8441c0c580374f42c8f4d71e15383a503cc59d10
Alex <deja@Alexs-MacBook-Air.local>
2021-04-04 14:15:43 -0700

    Instant Commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ const cubed = x => {
 const App = () => {
   return (
     <div className="tc bg-gray">
-      <h1 className="bg-black dib br3 pa2 ma2 grow bw2 shadow-5">
-        <p style={{color: 'white'}}>Alejandro Blanco</p>
+      <h1 className="bg-black dib br3 pa2 ma2 grow bw2 shadow-5" style={{color: 'white'}}>
+        <p>Alejandro Blanco</p>
+        <p>Hello world</p>
       </h1>
     </div>
   );
