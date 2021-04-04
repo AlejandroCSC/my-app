0661cee4c49234f51983fe98130af87985cd20e7
Alejandro <alejandrocsc@protonmail.com>
2021-04-04 11:07:54 -0700

    Instant Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import Stars from "./Drawings/Stars";
+const cubed = (x) => {return x*x*x;}
+
 const App = () => {
   return (
     <div className='tc'>
       <h1>Hello World</h1>
       <p>What're you doing?</p>
       <p>This actually worked!</p>
-      <p>How about this?</p>
+      <p>How about this? {cubed(20)}</p>
     </div>
   );
 }
