db8c6302c3a03e4758a259c1c09b98e0c1a89e96
Alejandro <alejandrocsc@protonmail.com>
2021-04-15 07:42:39 -0700

    Added site redirects

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-// import Stars from "./Drawings/Stars";
-const cubed = x => {
-  return x * x * x;
-};
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
 
 const App = () => {
   return (
-    <div className='tc bg-gray'>
-      <h1
-        className='bg-black dib br3 pa2 ma2 grow bw2 shadow-5'
-        style={{ color: "white" }}
-      >
-        <p>Alejandro Blanco</p>
-        <p>Hello world</p>
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
+        <Route component={SinglePost} path='/post/:slug' />
+        <Route component={Post} path='/post' />
+        <Route component={Project} path='/project' />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
