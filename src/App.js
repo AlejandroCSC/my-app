b7ca3cf485b69a5a6082b66f038089cdae0637d2
Alejandro <alejandrocsc@protonmail.com>
2021-04-15 07:21:03 -0700

    Added sanity.io configurations

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ const cubed = x => {
 
 const App = () => {
   return (
-    <div className="tc bg-gray">
-      <h1 className="bg-black dib br3 pa2 ma2 grow bw2 shadow-5" style={{color: 'white'}}>
+    <div className='tc bg-gray'>
+      <h1
+        className='bg-black dib br3 pa2 ma2 grow bw2 shadow-5'
+        style={{ color: "white" }}
+      >
         <p>Alejandro Blanco</p>
         <p>Hello world</p>
       </h1>
