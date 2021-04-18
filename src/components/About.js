17043cff126e87156ca1b4703c563cf42e3bfef8
Alejandro <alejandrocsc@protonmail.com>
2021-04-18 10:06:56 -0700

    Updated the dimensions for the background picture to be full width/height

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,11 @@ export default function About() {
   if (!author) return <div>Loading...</div>;
   return (
     <main className='relative'>
-      <img src={image} alt='alan-veas-unsplash' className='absolute w-full' />
+      <img
+        src={image}
+        alt='alan-veas-unsplash'
+        className='absolute w-full h-full'
+      />
       <div className='p-10 lg:pt-48 container mx-auto relative'>
         <section className='bg-blue-800 rounded-lg shadow-2xl lg:flex p-20'>
           <img
