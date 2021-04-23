5c3cd2193e9b9dd052307b10ec4d4c5ca54c3265
Alejandro <alejandrocsc@protonmail.com>
2021-04-23 10:51:59 -0700

    Fixed rerouting issue

--- src/components/Post.js
+++ src/components/Post.js
@@ -36,10 +36,7 @@ export default function Post() {
           {postData &&
             postData.map((post, index) => (
               <article>
-                <Link
-                  to={"portfolio/post/" + post.slug.current}
-                  key={post.slug.current}
-                >
+                <Link to={"post/" + post.slug.current} key={post.slug.current}>
                   <span
                     className='block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-blue-400'
                     key={index}
